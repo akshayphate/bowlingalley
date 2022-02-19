4269c2309204000f8350ac3bd46fda0dc3f1fafb
Akshay Phate <akshayphate@gmail.com>
2022-02-19 18:04:24 +0530

    refactor package publisher

--- src/com/bowling/alley/util/Queue.java
+++ src/com/bowling/alley/util/Queue.java
@@ -1,40 +1,43 @@
-package com.bowling.alley.util;/* Queue.java
+package com.bowling.alley.util;
+/* Queue.java
  *
  *  Version
  *  $Id$
- * 
+ *
  *  Revisions:
  * 		$Log$
- * 
+ *
  */
- 
+
 import java.util.Vector;
- 
-public class Queue {
-	private Vector v;
-	
+
+public class Queue<T>
+{
+	private Vector<T> v;
+
 	/** Queue()
-	 * 
+	 *
 	 * creates a new queue
 	 */
-	public Queue() {
-		v = new Vector();
+	public Queue()
+	{
+		v = new Vector<>();
 	}
-	
-	public Object next() {
+
+	public T next() {
 		return v.remove(0);
 	}
 
-	public void add(Object o) {
+	public void add(T o) {
 		v.addElement(o);
 	}
-	
+
 	public boolean hasMoreElements() {
 		return v.size() != 0;
 	}
 
-	public Vector asVector() {
+	public Vector<T> asVector() {
 		return v;
 	}
-	
+
 }
