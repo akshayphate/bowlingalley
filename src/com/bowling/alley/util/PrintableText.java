487553422e350cf03537d7060390dc85729c9c41
Akshay Phate <akshayphate@gmail.com>
2022-02-19 18:11:03 +0530

    refactor utility classes

--- src/com/bowling/alley/util/PrintableText.java
+++ src/com/bowling/alley/util/PrintableText.java
@@ -1,27 +1,36 @@
-package com.bowling.alley.util; /**
+package com.bowling.alley.util;
+/**
  *
  */
 
-import java.awt.*;
-import java.awt.print.*;
-import java.awt.geom.*;
-import java.awt.font.*;
-import java.text.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
+import java.awt.geom.Point2D;
+import java.awt.print.PageFormat;
+import java.awt.print.Printable;
 
-public class PrintableText implements Printable {
+public class PrintableText implements Printable
+{
 	String text;
 	int POINTS_PER_INCH;
 
-	public PrintableText(String t) {
+	public PrintableText(String t)
+	{
 		POINTS_PER_INCH = 72;
 		text = t;
 	}
 
-	public int print(Graphics g, PageFormat pageFormat, int pageIndex) {
-		if (pageIndex > 0) {
+	public int print(Graphics g, PageFormat pageFormat, int pageIndex)
+	{
+		if (pageIndex > 0)
+		{
 			return NO_SUCH_PAGE;
 		}
-		
+
 		Graphics2D g2d = (Graphics2D) g; // Allow use of Java 2 graphics on
 
 		g2d.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
@@ -30,12 +39,14 @@ public class PrintableText implements Printable {
 		Point2D.Double pen = new Point2D.Double(0.25 * POINTS_PER_INCH, 0.25 * POINTS_PER_INCH);
 
 		Font font = new Font ("courier", Font.PLAIN, 12);
-   		FontRenderContext frc = g2d.getFontRenderContext();
+		FontRenderContext frc = g2d.getFontRenderContext();
 
 		String lines[] = text.split("\n");
 
-		for (int i=0; i < lines.length; i++) {		
-			if (lines[i].length() > 0) {
+		for (int i=0; i < lines.length; i++)
+		{
+			if (lines[i].length() > 0)
+			{
 				TextLayout layout = new TextLayout(lines[i], font, frc);
 				layout.draw(g2d, (float) pen.x, (float) (pen.y + i*14));
 			}
--- src/com/bowling/alley/util/ScoreReport.java
+++ src/com/bowling/alley/util/ScoreReport.java
@@ -1,71 +1,86 @@
-package com.bowling.alley.util; /**
- * 
+package com.bowling.alley.util;
+/**
+ *
  * SMTP implementation based on code by R�al Gagnon mailto:real@rgagnon.com
- * 
+ *
  */
 
 
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Vector;
+
 import com.bowling.alley.model.Bowler;
 import com.bowling.alley.model.Score;
-import com.bowling.alley.util.PrintableText;
-import com.bowling.alley.util.ScoreHistoryFile;
 
-import java.io.*;
-import java.util.Vector;
-import java.util.Iterator;
-import java.net.*;
-import java.awt.print.*;
-
-public class ScoreReport {
-
+public class ScoreReport
+{
 	private String content;
-	
-	public ScoreReport(Bowler bowler, int[] scores, int games ) {
+
+	public ScoreReport(Bowler bowler, int[] scores, int games)
+	{
 		String nick = bowler.getNick();
 		String full = bowler.getFullName();
-		Vector v = null;
-		try{
+		Vector<Score> v = null;
+
+		try
+		{
 			v = ScoreHistoryFile.getScores(nick);
-		} catch (Exception e){System.err.println("Error: " + e);}
-		
-		Iterator scoreIt = v.iterator();
-		
-		content = "";
-		content += "--Lucky Strike Bowling Alley Score Report--\n";
-		content += "\n";
-		content += "Report for " + full + ", aka \"" + nick + "\":\n";
-		content += "\n";
-		content += "Final scores for this session: ";
-		content += scores[0];
-		for (int i = 1; i < games; i++){
-			content += ", " + scores[i];
 		}
-		content += ".\n";
-		content += "\n";
-		content += "\n";
-		content += "Previous scores by date: \n";
-		while (scoreIt.hasNext()){
-			Score score = (Score) scoreIt.next();
+		catch (Exception e)
+		{
+			System.err.println("Error: " + e);
+		}
+
+		content = generateContent(full, nick, scores, v);
+	}
+
+	private String generateContent(String fullName, String nickName, int[] scores, Vector<Score> v)
+	{
+		StringBuilder contentBuilder = new StringBuilder();
+
+		contentBuilder.append("");
+		contentBuilder.append("--Lucky Strike Bowling Alley Score Report--\n");
+		contentBuilder.append("\n");
+		contentBuilder.append("Report for " + fullName + ", aka \"" + nickName + "\":\n");
+		contentBuilder.append("\n");
+		contentBuilder.append("Final scores for this session: ");
+		contentBuilder.append(scores[0]);
+
+		for (int i = 1; i < scores.length; i++)
+		{
+			contentBuilder.append(", ").append(scores[i]);
+		}
+
+		contentBuilder.append(".\n\n\n");
+		contentBuilder.append("Previous scores by date: \n");
+
+		for (Score score : v)
+		{
 			content += "  " + score.getDate() + " - " +  score.getScore();
 			content += "\n";
 		}
-		content += "\n\n";
-		content += "Thank you for your continuing patronage.";
 
+		contentBuilder.append("\n\n");
+		contentBuilder.append("Thank you for your continuing patronage.");
+
+		return contentBuilder.toString();
 	}
 
-	public void sendEmail(String recipient) {
-		try {
-			Socket s = new Socket("osfmail.rit.edu", 25);
-			BufferedReader in =
-				new BufferedReader(
-					new InputStreamReader(s.getInputStream(), "8859_1"));
-			BufferedWriter out =
-				new BufferedWriter(
-					new OutputStreamWriter(s.getOutputStream(), "8859_1"));
+	public void sendEmail(String recipient)
+	{
 
-			String boundary = "DataSeparatorString";
-
+		try (
+				Socket s = new Socket("osfmail.rit.edu", 25);
+				BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream(), "8859_1"));
+				BufferedWriter out = new BufferedWriter(new OutputStreamWriter(s.getOutputStream(), "8859_1"));
+		)
+		{
 			// here you are supposed to send your username
 			sendln(in, out, "HELO world");
 			sendln(in, out, "MAIL FROM: <mda2376@rit.edu>");
@@ -81,46 +96,60 @@ public class ScoreReport {
 			sendln(in, out, ".");
 			sendln(in, out, "QUIT");
 			s.close();
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			e.printStackTrace();
 		}
 	}
 
-	public void sendPrintout() {
+	public void sendPrintout()
+	{
 		PrinterJob job = PrinterJob.getPrinterJob();
-
 		PrintableText printobj = new PrintableText(content);
 
 		job.setPrintable(printobj);
 
-		if (job.printDialog()) {
-			try {
+		if (job.printDialog())
+		{
+			try
+			{
 				job.print();
-			} catch (PrinterException e) {
+			}
+			catch (PrinterException e)
+			{
 				System.out.println(e);
 			}
 		}
 
 	}
 
-	public void sendln(BufferedReader in, BufferedWriter out, String s) {
-		try {
+	public void sendln(BufferedReader in, BufferedWriter out, String s)
+	{
+		try
+		{
 			out.write(s + "\r\n");
 			out.flush();
 			// System.out.println(s);
 			s = in.readLine();
 			// System.out.println(s);
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			e.printStackTrace();
 		}
 	}
 
-	public void sendln(BufferedWriter out, String s) {
-		try {
+	public void sendln(BufferedWriter out, String s)
+	{
+		try
+		{
 			out.write(s + "\r\n");
 			out.flush();
 			System.out.println(s);
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			e.printStackTrace();
 		}
 	}
