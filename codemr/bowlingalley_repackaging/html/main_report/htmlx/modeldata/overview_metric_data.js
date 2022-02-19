2e1e44bfc410aa4d648431f89b2e2a5ad8ed7f83
Akshay Phate <akshayphate@gmail.com>
2022-02-19 17:35:51 +0530

    code metrics after repackaging

---
+++ codemr/bowlingalley_repackaging/html/main_report/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"bowlingalley", "Number of Highly Problematic Classes":0, "Number of Entities":27, "Number of Problematic Classes":0, "Class Lines of Code":1429, "Number of External Packages":18, "Number of Packages":7, "Number of External Entities":91};var EQ_COLOR_OF_LEVELS = ["#1F77B4","#007F24","#62BF18","#FFC800","#FF5B13","#E50000"];
